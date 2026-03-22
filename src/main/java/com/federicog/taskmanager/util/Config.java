da1d9bb910c1bec8bb212367ac299dce60b10da0
Federico Galan <federico.egv@gmail.com>
2026-03-22 15:02:36 -0300

    feat: add Config constants

---
+++ src/main/java/com/federicog/taskmanager/util/Config.java
@@ -0,0 +1,9 @@
+package com.federicog.taskmanager.util;
+
+public final class Config {
+
+    public static final String SCHEMA_PATH = "data/tasks-schema.json";
+    public static final String FILE_PATH = "data/tasks.json";
+
+    private Config() {}
+}
